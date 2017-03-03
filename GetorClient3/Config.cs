fd302ddf025ec2c57f961a23134faf0a0fca1bd0
Fabio Ponte <fabio@erpsolutions.com.br>
2017-03-03 08:15:39 -0300

    Add project files.

---
+++ GetorClient3/Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace GetorClient3
+{
+    class Config
+    {
+        static public string local = "";
+
+        static public void Obterlocal()
+        {
+            string l = Environment.CurrentDirectory + "\\";
+            if (File.Exists(l + "config.ianez"))
+            {
+                StreamReader leitor = new StreamReader(l + "config.ianez");
+                local = leitor.ReadLine();
+                leitor.Close();
+            }
+
+        }
+    }
+}
---
+++ GetorClient3/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+namespace GetorClient3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string cd = "RESENDE";
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Config.Obterlocal();
+        }
+
+        private void radioButton2_Checked(object sender, RoutedEventArgs e)
+        {
+            cd = "XANXERE";
+        }
+
+        private void radioButton3_Checked(object sender, RoutedEventArgs e)
+        {
+            cd = "PASSO FUNDO";
+        }
+
+        private void radioButton4_Checked(object sender, RoutedEventArgs e)
+        {
+            cd = "PATO BRANCO";
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox1.Text.Trim() != "")
+            {
+                char a = char.Parse(",");
+                string[] notas = textBox1.Text.Split(a);
+                StreamWriter escreitor = new StreamWriter(Config.local + "" + DateTime.Now.Second + "" + DateTime.Now.Millisecond + ".ianez");
+                escreitor.WriteLine(cd);
+
+                for (int ed = 0; ed < notas.Length; ed++)
+                {
+                    escreitor.WriteLine(notas[ed]);
+                }
+                escreitor.Close();
+                textBox1.Text = "";
+            }
+        }
+
+        private void radioButton1_Checked(object sender, RoutedEventArgs e)
+        {
+            cd = "RESENDE";
+        }
+    }
+}
